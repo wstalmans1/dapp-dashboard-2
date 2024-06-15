cbf0c6e3d9838d5cf1acebfcaa0e51725602d252
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-15 06:51:28 +0200

    some css harmonisation

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,8 +4,8 @@ import { NavLink, Outlet, Link } from "react-router-dom";
 
 export default function Dashboard() {
   return (
-    <div className="min-h-screen flex flex-col p-5 md:flex-row bg-gradient-to-british-racing-green">
-      <div className="flex flex-col gap-2 flex-shrink-0">
+    <div className="min-h-screen flex flex-col p-4 md:flex-row bg-gradient-to-british-racing-green">
+      <div className="flex flex-col gap-2 flex-shrink-0 m-2">
         <div className="mb-2 flex flex-col h-20 items-end justify-between rounded-md bg-gradient-to-r from-british-racing-green to-rich-gold p-4 md:h-40">
           <div className="w-32 text-white md:w-40">Team Blockchain Advisors</div>
           <div className="w-32 text-gray-400 md:w-40">Dashboard</div>
@@ -28,8 +28,14 @@ export default function Dashboard() {
           <div className="hidden md:block">Sign Out</div>
         </button>
       </div>
-      <div className="flex-grow">
-        <Outlet />
+      <div className='flex-grow flex flex-col flex-shrink-0 m-2'>
+        <div className="flex items-center justify-center mb-1 bg-white bg-opacity-5 w-full rounded-lg p-2">
+          <div className="mb-2">Connection Status: {/* Add connection status here */}</div>
+          <button className="px-4 py-2 bg-blue-500 text-white rounded">Connect to MetaMask</button>
+        </div>
+        <div className="flex-grow bg-white bg-opacity-5 w-full rounded-lg mt-1">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- src/components/PartySplit.tsx
+++ src/components/PartySplit.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const PartySplit: React.FC = () => (
-  <div className="flex grow w-full h-full flex-col px-3 md:px-2">
-    <div className="flex grow flex-row justify-between space-x-2 bg-white bg-opacity-5 w-full rounded-lg md:flex-col md:space-x-0 md:space-y-2">
+  <div className="flex grow w-full h-full flex-col">
+    <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
       <h1>Split the Party</h1>
       <p>This is the content for Partysplit.</p>
     </div>
