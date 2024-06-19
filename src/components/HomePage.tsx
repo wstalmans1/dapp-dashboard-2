f621da6bc7c929014c0a99645d64263f94dbc8f7
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-19 23:33:41 +0200

    homepage text

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,11 +6,11 @@ export default function HomePage() {
       <div className="min-h-screen flex flex-col justify-center p-5 bg-gradient-to-british-racing-green text-gray-50 items-center">
         <h1 className="text-4xl mb-4 p-10 flex flex-col justify-center h-20 md:h-40 items-stretch rounded-md bg-gradient-to-r from-british-racing-green to-rich-gold">Team Blockchain Advisors</h1>
         <p className="text-lg pt-6 mb-4 text-center">Welcome to your DAO. <br />
-          To get started, navigate to the dashboard.</p>
-        <Link to="/dashboard" className="text-blue-500 hover:underline">Go to Dashboard</Link>
+          To get started, <Link to="/dashboard" className="text-red-300 hover:underline">go to the Dashboard</Link>.</p>
         <div className="flex flex-col list-disc mt-10 ml-20">
+          <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Realise and sell products</p>
           <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Mint tokens by building your reputation</p>
-          <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Build reputation by advancing the DAO</p>
+          <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Build reputation by your expertise</p>
           <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Sell tokens to clients to pay for services</p>
           <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Adhere to the DAO Constitution</p> 
           <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Govern the DAO's Constitution and operations</p>
