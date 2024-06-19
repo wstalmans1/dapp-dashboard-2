c3433977f1d05ca564f3435e31eff32b27db6a4a
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-20 00:13:36 +0200

    no provider alert 2

--- src/utils/connectiondot.tsx
+++ src/utils/connectiondot.tsx
@@ -23,13 +23,11 @@ const ConnectionDot: React.FC = () => {
                         </div>
                     </div>    
             ) : (
-                    
                 <div className="flex flex-col pb-2">    
                         <div className="flex items-center">                
                             <div className="mr-2 h-3 w-3 bg-red-500 rounded-full"></div>
                             <span className="text-sm">Not Connected</span>    
-                            <p>No provider detected. Install MetaMask or Phantom wallet.</p>
-                            <button onClick={connectWallet} className="bg-green-500 text-green-100 text-xs px-1 py-1 rounded ml-2">Connect</button>
+                            <p className="pl-2 text-sm">No provider detected. Install MetaMask or Phantom wallet.</p>
                         </div>
                     </div>
                 
