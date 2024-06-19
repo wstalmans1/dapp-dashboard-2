cd2d2b6a101570c7a6b8d8b68831daf99472b8fa
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-19 23:46:16 +0200

    homepage text

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,13 +5,12 @@ export default function HomePage() {
     return (
       <div className="min-h-screen flex flex-col justify-center p-5 bg-gradient-to-british-racing-green text-gray-50 items-center">
         <h1 className="text-4xl mb-4 p-10 flex flex-col justify-center h-20 md:h-40 items-stretch rounded-md bg-gradient-to-r from-british-racing-green to-rich-gold">Team Blockchain Advisors</h1>
-        <p className="text-lg pt-6 mb-4 text-center">Welcome to your DAO. <br />
-          Start managing and contributing.<br />
-          <Link to="/dashboard" className="text-red-300 hover:underline">go to the Dashboard</Link>.</p>
+        <p className="text-lg pt-6 text-center">Welcome to your DAO. <br />
+          Start managing and contributing:<br />
+          <Link to="/dashboard" className="text-red-300 hover:underline">Go to the Dashboard</Link>.</p>
         <div className="flex flex-col list-disc mt-10 ml-20">
-          <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Realise, sell and contribute to products</p>
+          <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Build your reputation by realising, selling and contributing to products</p>
           <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Mint tokens by building your reputation</p>
-          <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Build reputation through your expertise</p>
           <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Sell tokens to clients to pay for services</p>
           <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Adhere to the DAO Constitution</p> 
           <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Govern the DAO's Constitution and operations</p>
