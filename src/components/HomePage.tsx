d13a19471bfbe2879ebd426c3839efe6ff4093ff
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-19 22:40:38 +0200

    additional formating homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
         <h1 className="text-4xl mb-4">Team Blockchain Advisors</h1>
         <p className="text-lg mb-4 text-center">Welcome to your application. To get started, navigate to the dashboard.</p>
         <Link to="/dashboard" className="text-blue-500 hover:underline">Go to Dashboard</Link>
-        <ul className="flex flex-col list-disc mt-10 ml-20">
+        <ul className="flex flex-col list-disc mt-10 ml-20 items-center">
           <li>You mint tokens by building your reputation. Tokens are needed by clients to pay for services proposed by the TBA DAO.</li>
           <li>You adhere to the DAO Constitution, by participating. You can change the Constitution</li>
           <li>You can change the DAO Constitution by particpating in the DAO Governance</li>
