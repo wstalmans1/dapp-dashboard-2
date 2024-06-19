4a4ef98d7366be27a42248a630ea0af173f2a939
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-19 23:19:15 +0200

    homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,8 +4,8 @@ import '../styles/main.css';
 export default function HomePage() {
     return (
       <div className="min-h-screen flex flex-col justify-center p-5 bg-gradient-to-british-racing-green text-gray-50 items-center">
-        <h1 className="text-4xl mb-4">Team Blockchain Advisors</h1>
-        <p className="text-lg mb-4">Welcome to your application. To get started, navigate to the dashboard.</p>
+        <h1 className="text-4xl mb-4 p-10 flex flex-col justify-center h-20 md:h-40 items-stretch rounded-md bg-gradient-to-r from-british-racing-green to-rich-gold">Team Blockchain Advisors</h1>
+        <p className="text-lg pt-6 mb-4">Welcome to your application. To get started, navigate to the dashboard.</p>
         <Link to="/dashboard" className="text-blue-500 hover:underline">Go to Dashboard</Link>
         <div className="flex flex-col list-disc mt-10 ml-20">
           <p style={{ paddingLeft: '1em', textIndent: '-1em' }} className="pb-3">• Mint tokens by building your reputation</p>
